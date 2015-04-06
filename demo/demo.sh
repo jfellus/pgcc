5f884fa445181769c8f9a1b4ae0ce8b23ba826ba
Jerome Fellus <jfellus@gmail.com>
2015-04-06 23:58:46 +0200

    v1.0

---
+++ demo/demo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ..
+echo "pgcc demo/main.script demo/out"
+
+./pgcc demo/main.script demo/out
+
+echo
+echo " Hey ! Thank you for using pgcc demo ! Please find your generated project in ./out "
+echo " Bye."
